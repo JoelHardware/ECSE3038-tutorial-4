63fee1108fed6321d53b872a1212a00397f015bf
iamjoel12 <joelhardware777@gmail.com>
2026-09-23 16:02:48 -0500

    task 2: update device

--- app.py
+++ app.py
@@ -38,3 +38,12 @@ def create_device(device: Device):
     new_device = device.model_dump()
     readings.append(new_device)
     return new_device
+
+# Put request to update a device
+@app.put("/devices/{name}")
+def update_device(name: str, device: Device):
+    for i, d in enumerate(readings):
+        if d["name"] == name:
+            readings[i] = device.model_dump()
+            return readings[i]
+    raise HTTPException(status_code=404, detail="No device called " + name)
